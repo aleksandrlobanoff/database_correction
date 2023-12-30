b780d3b21ae339056245eb67b4da4036b0fce905
alexandr.lobanov <alexandp83@mail.ru>
2023-12-30 14:18:08 +0300

    Реализована коррекция таблиц второй_БД по примеру первой_БД

--- database_correction.py
+++ database_correction.py
@@ -21,3 +21,31 @@ class DatabaseCorrection:
         first_db_connection = self.create_connection(self.first_db_config)
         second_db_connection = self.create_connection(self.second_db_config)
 
+        # Создание курсоров для работы с базами данных
+        first_db_cursor = first_db_connection.cursor()
+        second_db_cursor = second_db_connection.cursor()
+
+        # Получение списка таблиц из первой базы данных
+        first_db_cursor.execute("SHOW TABLES")
+        first_tables = [row[0] for row in first_db_cursor.fetchall()]
+
+        # Получение списка таблиц из второй базы данных
+        second_db_cursor.execute("SHOW TABLES")
+        second_tables = [row[0] for row in second_db_cursor.fetchall()]
+
+        # Коррекция базы данных
+        # Добавление новых таблиц
+        new_tables = list(set(first_tables) - set(second_tables))
+        for table in new_tables:
+            # Получение структуры таблицы из первой базы данных
+            first_db_cursor.execute(f"SHOW CREATE TABLE {table}")
+            create_table_query = first_db_cursor.fetchone()[1]
+
+            # Создание новой таблицы во второй базе данных по образцу из первой базы данных
+            second_db_cursor.execute(create_table_query)
+
+            second_db_connection.commit()
+
+        # Закрытие соединений
+        first_db_connection.close()
+        second_db_connection.close()
